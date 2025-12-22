36ff64ee0c5c528b7f940da57fe5e90de8315e2a
Gronsak <dev@gronsak.tech>
2025-12-22 20:53:07 +0100

    Added a fancy ascii text banner and a running time indicator for each output

--- flagfetcher.py
+++ flagfetcher.py
@@ -3,6 +3,7 @@ from datetime import datetime as time
 
 ServerUrl = "http://10.3.10.104:3000"
 s = requests.session()
+start = time.now()
 
 class FlagToken:
     def __init__(self, token:str, expiresIn: int, verifyWithin: int, claimWithin: int):
@@ -77,25 +78,35 @@ def fetch_flag(token: FlagToken) -> str:
     flag = json["flag"]
     return flag
 
+def running_time() -> float:
+    return (time.now() - start).microseconds/1000
+
 def main():
+    print(r""" _______ _                 ___                  _                    ___     _____  
+(_______) |               / __)        _       | |                  (___)   (_____) 
+ _____  | | _____  ____ _| |__ _____ _| |_ ____| |__  _____  ____      _    _  __ _ 
+|  ___) | |(____ |/ _  (_   __) ___ (_   _) ___)  _ \| ___ |/ ___)    | |  | |/ /| |
+| |     | |/ ___ ( (_| | | |  | ____| | |( (___| | | | ____| |       _| |_ |   /_| |
+|_|      \_)_____|\___ | |_|  |_____)  \__)____)_| |_|_____)_|      (_____|_)_____/ 
+                 (_____|                                                            """)
     try:
-        print("Requesting token...")
+        print(f"({running_time()}ms) Requesting token...")
         token = fetch_token()
         if token.token != "":
-            print("Token recieved!")
+            print(f"({running_time()}ms) Token recieved!")
         else:
             print("No Token recieved, exiting!")
             return
         
-        print("Verifying token...")
+        print(f"({running_time()}ms) Verifying token...")
         token = verify_token(token)
         if token.secret != "":
-            print("Token verified!")
+            print(f"({running_time()}ms) Token verified!")
         else:
             print("Token was not verified but no errors was raised, exiting!")
             return
         
-        print("Requesting flag...")
+        print(f"({running_time()}ms) Requesting flag...")
         flag = fetch_flag(token)
     except ConnectionError as e:
         print(e)
@@ -108,7 +119,7 @@ def main():
         return
     
     if flag != "":
-        print("Flag retrived:")
+        print(f"({running_time()}ms) Flag retrived:")
         print(flag)
     else:
         print("No flag retrived for unknown reason, exiting!")
